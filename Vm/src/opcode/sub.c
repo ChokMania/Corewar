16b75033e1cd0f12b302a2cad7e2f7a7beedb7e4
Julien DUMAY <judumay@e2r2p18.42.fr>
2019-09-26 12:31:06 +0200

    maj opcode arg_value et memsize

--- Vm/src/opcode/sub.c
+++ Vm/src/opcode/sub.c
@@ -3,25 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   sub.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mabouce <mabouce@student.42.fr>            +#+  +:+       +#+        */
+/*   By: judumay <judumay@42.student.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 10:26:04 by anmauffr          #+#    #+#             */
-/*   Updated: 2019/09/25 15:33:01 by mabouce          ###   ########.fr       */
+/*   Updated: 2019/09/26 12:28:48 by judumay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-static void	ft_arg(t_vm *vm, unsigned int *pc, unsigned int *arg_value)
+static int	ft_arg(t_vm *vm, unsigned int *pc, unsigned int *arg_value)
 {
 	int		i;
+	int		r;
 
 	i = 0;
+	r = 1;
 	while (i < 3)
 	{
 		(*pc) += T_REG;
+		(*pc) %= MEM_SIZE;
 		arg_value[i++] = vm->arena[*pc][0] - 0x01;
+		if (arg_value[i - 1] > 15)
+			r = 0;
 	}
+	return (r);
 }
 
 static void	exec_sub(t_vm *vm, unsigned int arg_value[3])
@@ -36,7 +42,9 @@ void		op_sub(t_vm *vm, unsigned int *pc)
 {
 	unsigned int	arg_value[3];
 
-	ft_arg(vm, pc, arg_value);
-	exec_sub(vm, arg_value);
-	ft_visu_d_message(vm, "sub");
+	if (ft_arg(vm, pc, arg_value))
+	{
+		exec_sub(vm, arg_value);
+		ft_visu_d_message(vm, "sub");
+	}
 }

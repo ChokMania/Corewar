9ca9d4a3cc8cc1f27bf1871b309081c4e9b2bfd6
Antoine MAUFFRET <anmauffr@e2r3p15.42.fr>
2019-09-26 14:44:27 +0200

    mini modif ldi

--- Vm/src/opcode/ldi.c
+++ Vm/src/opcode/ldi.c
@@ -6,7 +6,7 @@
 /*   By: anmauffr <anmauffr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 10:25:30 by anmauffr          #+#    #+#             */
-/*   Updated: 2019/09/26 14:39:42 by anmauffr         ###   ########.fr       */
+/*   Updated: 2019/09/26 14:44:05 by anmauffr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,15 @@ static int	ft_arg(t_vm *vm, unsigned int *pc, unsigned int *arg_value,
 		if (arg_size[i] == T_REG)
 		{
 			(*pc) = (*pc + T_REG) % MEM_SIZE;
-			arg_size[i] = T_REG;
 			arg_value[i] = vm->arena[*pc][0] - 0x01;
 			if (arg_value[i] > 15)
 				ret = 0;
 		}
-		else if (arg_size[i] == T_DIR)
+		else if (arg_size[i] == T_DIR || arg_size[i] == T_IND)
 		{
 			(*pc) = (*pc + T_DIR) % MEM_SIZE;
-			arg_size[i] = T_DIR;
-			arg_value[i] = vm->arena[(*pc - 1) % MEM_SIZE][0] << 8 | vm->arena[*pc][0];
-		}
-		else if (arg_size[i] == T_IND)
-		{
-			(*pc) = (*pc + T_DIR) % MEM_SIZE;
-			arg_size[i] = T_IND;
-			arg_value[i] = vm->arena[*pc - 1][0] << 8 | vm->arena[*pc][0];
+			arg_value[i] = vm->arena[(*pc - 1) % MEM_SIZE][0] << 8
+				| vm->arena[*pc][0];
 		}
 	return (0);
 }

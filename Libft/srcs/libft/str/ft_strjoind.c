794563b1741ae15572c46da2b0992509efa5c813
Mabouce <mabouce@student.42.fr>
2019-10-02 16:10:38 +0200

    update strdjoin

--- Libft/srcs/libft/str/ft_strjoind.c
+++ Libft/srcs/libft/str/ft_strjoind.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdjoin.c                                      :+:      :+:    :+:   */
+/*   ft_strjoind.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mabouce <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mabouce <mabouce@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 13:49:37 by mabouce           #+#    #+#             */
-/*   Updated: 2019/01/17 12:46:35 by mabouce          ###   ########.fr       */
+/*   Updated: 2019/10/02 16:09:54 by mabouce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strjoind(char const *s1, char *s2)
 	if (s2)
 	{
 		if (!(str = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
-			exit(0);
+			return (NULL);
 		ft_strcat(str, s1);
 		ft_strcat(str, s2);
 		ft_strdel(&s2);

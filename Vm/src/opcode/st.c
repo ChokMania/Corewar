12d4915a272a09becd5790189f5455899f7e9beb
Mabouce <mabouce@student.42.fr>
2019-10-02 12:21:54 +0200

    idx mode sur st et sti

--- Vm/src/opcode/st.c
+++ Vm/src/opcode/st.c
@@ -6,7 +6,7 @@
 /*   By: mabouce <mabouce@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 10:26:13 by anmauffr          #+#    #+#             */
-/*   Updated: 2019/10/01 17:02:32 by mabouce          ###   ########.fr       */
+/*   Updated: 2019/10/02 12:21:38 by mabouce          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,13 +44,15 @@ static void	exec_st(t_vm *vm, unsigned int arg_value[3],
 	else if (arg_size[1] == T_IND)
 		index += arg_value[1] - T_DIR;
 	index += vm->proc->pc - 2;
-	realpc = (vm->proc->pc - 2 - arg_size[1]) % MEM_SIZE;
+	realpc = (vm->proc->pc - arg_size[0] - arg_size[1]) % MEM_SIZE;
 	index %= MEM_SIZE;
 	//idx mode
 	if ((index < realpc && realpc - index <= IDX_MOD) || (index > realpc && index - realpc <= IDX_MOD) || (index > realpc && realpc + MEM_SIZE - index <= IDX_MOD) || (index < realpc && index + MEM_SIZE - realpc <= IDX_MOD))
 		;
-	else
-		index %= IDX_MOD;
+	else if (index < realpc)
+		index = realpc - (realpc - index) % IDX_MOD;
+	else if (index > realpc)
+		index = (realpc + (index % IDX_MOD) - 1) % MEM_SIZE;
 	// fin idx mode
 	//INVALID READ DE ST
 	if (arg_size[1] == T_REG)

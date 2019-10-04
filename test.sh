cc7c8101a981ef3985257acdceaef9002a32f1b5
Mabouce <mabouce@student.42.fr>
2019-10-04 15:32:40 +0200

    better test.sh

--- test.sh
+++ test.sh
@@ -4,25 +4,13 @@ champ1="Vm/test/testmalo.s"
 ./asm $champ1
 champ1="Vm/test/testmalo.cor"
 
-champ2="Vm/test/testmalo.s"
+champ2="Vm/test/Bazou_test.s"
 ./asm $champ2
-champ2="Vm/test/testmalo.cor"
+champ2="Vm/test/Bazou_test.cor"
 
-champ3="Vm/test/testmalo.s"
+champ3="vm_champs/champs/examples/helltrain.s"
 ./asm $champ3
-champ3="Vm/test/testmalo.cor"
-
-champ4="Vm/test/testmalo.s"
-./asm $champ4
-champ4="Vm/test/testmalo.cor"
-
-champ5="Vm/test/testmalo.s"
-./asm $champ5
-champ5="Vm/test/testmalo.cor"
-
-champ6="Vm/test/testmalo.s"
-./asm $champ6
-champ6="Vm/test/testmalo.cor"
+champ3="vm_champs/champs/examples/helltrain.cor"
 
 while :
 do
@@ -43,19 +31,5 @@ do
 	./vm_champs/corewar $champ3 -d $INPUT > test2
 	diff test1 test2 | cat -e
 	echo "\$$champ3 OK"
-	
-	./corewar $champ4 -d $INPUT > test1
-	./vm_champs/corewar $champ4 -d $INPUT > test2
-	diff test1 test2 | cat -e
-	echo "\$$champ4 OK"
-	
-	./corewar $champ5 -d $INPUT > test1
-	./vm_champs/corewar $champ5 -d $INPUT > test2
-	diff test1 test2 | cat -e
-	echo "\$$champ5 OK"
-	
-	./corewar $champ6 -d $INPUT > test1
-	./vm_champs/corewar $champ6 -d $INPUT > test2
-	diff test1 test2 | cat -e
-	echo "\$$champ6 OK"
+
 done

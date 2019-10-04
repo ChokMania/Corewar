3559f26b4f30873dc2acd506262d74131e990b0c
Julien DUMAY <judumay@e2r9p11.42.fr>
2019-10-04 14:00:52 +0200

    maj

--- Vm/src/opcode/ldi.c
+++ Vm/src/opcode/ldi.c
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   ldi.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anmauffr <anmauffr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: judumay <judumay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 10:25:30 by anmauffr          #+#    #+#             */
-/*   Updated: 2019/10/02 13:41:56 by anmauffr         ###   ########.fr       */
+/*   Updated: 2019/10/04 14:00:52 by judumay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+static void	exec_ldi(t_vm *vm, unsigned int arg_value[3],
+	unsigned arg_size[3])
+{
+	int i;
+
+	if (arg_size[1] == T_REG)
+		arg_value[1] = vm->proc->r[arg_value[1]];
+	if (arg_size[0] == T_REG)
+		arg_value[0] = vm->proc->r[arg_value[0]];
+	i = (vm->proc->pc - (1 + arg_size[0] + arg_size[1] + arg_size[2])
+		+ (arg_value[0] + arg_value[1])) % MEM_SIZE;
+	vm->proc->r[arg_value[2]] = vm->arena[i % MEM_SIZE][0] << 24
+		| vm->arena[(i + 1) % MEM_SIZE][0] << 16
+		| vm->arena[(i + 2) % MEM_SIZE][0] << 8
+		| vm->arena[(i + 3) % MEM_SIZE][0];
+	vm->proc->carry = vm->proc->r[arg_value[2]] == 0 ? 1 : 0;
+}
 
 static void	exec_ldi(t_vm *vm, unsigned int arg_value[3],
 	unsigned arg_size[3])

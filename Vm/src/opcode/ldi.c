b0c09d9c530f8e74b0fc76fbe0bdee96ed569c31
Julien DUMAY <judumay@e2r9p11.42.fr>
2019-10-04 14:29:47 +0200

    ldi pushed

--- Vm/src/opcode/ldi.c
+++ Vm/src/opcode/ldi.c
@@ -6,7 +6,7 @@
 /*   By: judumay <judumay@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/28 10:25:30 by anmauffr          #+#    #+#             */
-/*   Updated: 2019/10/04 14:23:29 by judumay          ###   ########.fr       */
+/*   Updated: 2019/10/04 14:29:10 by judumay          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,7 @@
 static void	exec_ldi(t_vm *vm, unsigned int arg_value[3],
 	unsigned arg_size[3])
 {
-		int i;
-
-	if (arg_size[1] == T_REG)
-		arg_value[1] = vm->proc->r[arg_value[1]];
-	if (arg_size[0] == T_REG)
-		arg_value[0] = vm->proc->r[arg_value[0]];
-	i = (vm->proc->pc - (1 + arg_size[0] + arg_size[1] + arg_size[2])
-		+ (arg_value[0] + arg_value[1])) % MEM_SIZE;
-	vm->proc->r[arg_value[2]] = vm->arena[i % MEM_SIZE][0] << 24
-		| vm->arena[(i + 1) % MEM_SIZE][0] << 16
-		| vm->arena[(i + 2) % MEM_SIZE][0] << 8
-		| vm->arena[(i + 3) % MEM_SIZE][0];
-	vm->proc->carry = vm->proc->r[arg_value[2]] == 0 ? 1 : 0;
-	/*int		realpc;
+	int		realpc;
 	int		index;
 
 	index = 0;
@@ -52,7 +39,7 @@ static void	exec_ldi(t_vm *vm, unsigned int arg_value[3],
 		| vm->arena[idx_mod(realpc, index + 2)][0] << 8
 		| vm->arena[idx_mod(realpc, index + 3)][0];
 	vm->proc->carry = vm->proc->r[arg_value[2]] == 0 ? 1 : 0;
-*/}
+}
 
 
 void		op_ldi(t_vm *vm, unsigned int *pc)
